bb4583818e8ebe73cfdf20bb77f5c1c9d9c250fc
Oviyasri Elanchezhiyan <oviyasri02chezhiyan@gmail.com>
2025-12-19 11:54:46 +0530

    Add files via upload

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,18 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component, inject, input} from '@angular/core';
+import { InvestmentService } from '../user-input/investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+  private investmentService=inject(InvestmentService);
+  
+  get results() {
+    return this.investmentService.resultData;
+  }
+}
